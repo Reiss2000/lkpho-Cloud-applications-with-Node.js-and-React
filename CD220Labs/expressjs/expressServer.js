b29725ffea4de49591460604db6667f06ab3e48f
Reiss Davidson <reiss200@live.co.uk>
2021-07-10 06:51:32 +0100

    Added a feature that will return the corresponding month of the year to a number selected by the user after the /fetch directory

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -1,3 +1,11 @@
+// Run npm install, to install dependencies
+// In the terminal run the command 'node expressServer.js' to start the server
+// Open another terminal and run 'curl localhost:3333' to show "Welcome to the express server"
+// Run 'curl -X POST http://localhost:3333/login/Jason' to show "Jason, You are logged in!"
+// Run 'curl http://localhost:3333/Jason' to show "Hello Jason"
+// Run 'curl http://localhost:3333/loginDetails' to show "[{"name":"Jason","login_time":"..."}]"
+// Run 'curl http://localhost:3333/fetch/num' replacing {num} with a number 1-12 to get the corresponding month of the year "The month is ..."
+
 const express = require('express');
 const app = new express();
 
@@ -23,4 +31,10 @@ app.get("/:name",(req,res)=>{
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
+
+app.post("/fetch/:num",(req,res)=>{
+    const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    res.send("The month is " + months[req.params.num - 1])
+})
+
 
